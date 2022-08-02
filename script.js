5ed6cfb46f71b0409f818805d2e16a3e2de1c26d
ItzRae <rachellin443@gmail.com>
2022-08-02 15:39:18 -0400

    Created grid draw function and primary buttons for canvas

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
+const grid = document.querySelector(".grid");
+const color = document.querySelector(".color");
+const reset = document.querySelector(".reset")
 
+let draw = false;
 
 function makeGrid(pixels) {
-    const grid = document.querySelector(".grid");
     for (let i = 0; i < pixels * pixels; i++) {
         const pixel = document.createElement("div");
         pixel.classList.add("pixel");   
+        pixel.addEventListener('mouseover', function () {
+            if (!draw) return;
+            pixel.style.backgroundColor = color.value;
+        });
+        pixel.addEventListener('mousedown', function () {
+            pixel.style.backgroundColor = color.value;
+        });
+
         grid.appendChild(pixel);
     }
 
     grid.style.gridTemplateColumns = `repeat(${pixels}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${pixels}, 1fr)`;
 }
+
+window.addEventListener('mousedown', function() {
+    draw = true;
+});
+window.addEventListener('mouseup', function () {
+    draw = false;
+})
+
+
+reset.addEventListener('click', function() {
+    grid.innerHTML = '';
+    makeGrid(16);
+});
+
 
 makeGrid(16);
