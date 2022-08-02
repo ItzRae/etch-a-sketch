3813137656d7cbe562e887f17e25b6491b20e3e7
ItzRae <rachellin443@gmail.com>
2022-08-02 14:33:57 -0400

    Set up grid pixels and navbar structure

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 
 
 function makeGrid(pixels) {
-    const grid = document.querySelector("grid");
-   
+    const grid = document.querySelector(".grid");
+    for (let i = 0; i < pixels * pixels; i++) {
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");   
+        grid.appendChild(pixel);
+    }
+
+    grid.style.gridTemplateColumns = `repeat(${pixels}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${pixels}, 1fr)`;
 }
 
 makeGrid(16);
